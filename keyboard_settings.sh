f16fdbdf65b53708e75bda85149e4a18a5390719
Andreas Geisenhainer <psycorama@opensecure.de>
2012-01-16 18:50:39 +0100

    added support for 'imp tech tasta' hardware and added NEO_PATH variable

--- keyboard_settings.sh
+++ keyboard_settings.sh
@@ -22,16 +22,23 @@ xset -b
 #sets keyboard to 250ms timeout 30chars/s
 xset r rate 250 30
 #sets mouse to acceleration 2 with 30 timeout
-xset m 5 30
+xset m 2 30
+
+NEO_PATH=$HOME/scripte/neo
 
 if [ "$MODE" == "neo" ]; then
     touch /tmp/neo
-    setxkbmap lv
-    xmodmap $HOME/scripte/neo_de.xmodmap 
+    if [ -n "`xinput --list |grep \"imp tech tasta\"`" ]; then
+	setxkbmap de nodeadkeys
+    else
+	setxkbmap lv
+	xmodmap $NEO_PATH/neo_de.xmodmap
+    fi;
     xset -r 51
+
     if [ -z "`pidof neo_layout_viewer`" ]; then
 	#start view helper
-	cd $HOME/bin/ && ./neo_layout_viewer &
+	cd $NEO_PATH/ && ./neo_layout_viewer &
     fi;
 elif [ "$MODE" == "us" ]; then  
     if [ -e /tmp/neo ]; then
@@ -46,6 +53,7 @@ elif [ "$MODE" == "us" ]; then
     #xmodmap -e 'keycode 66 = Control_L'
     #xmodmap -e 'clear Lock'
     #xmodmap -e 'add Control = Control_L'
+
 elif [ "$MODE" == "off" ]; then
     if [ -n "`pidof neo_layout_viewer`" ]; then
 	killall neo_layout_viewer;
