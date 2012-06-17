c9bf028c072ce7299523dd90d4931ff54de5de21
Andreas Geisenhainer <psycorama@opensecure.de>
2012-06-17 15:33:05 +0200

    fixed bug occuring at 0% volume level, added checking for sensible volumes

--- osd_volume.sh
+++ osd_volume.sh
@@ -8,10 +8,17 @@
 sink=`pacmd info|grep "Default sink name"|awk '{print $4}'`
 
 # get volume
-volume=`pacmd list-sinks|grep "volume: 0:"|cut -d " " -f 4|sed -e 's/%//'`
+#volume=`pacmd list-sinks|grep "volume: 0:"|cut -d " " -f 4|sed -e 's/%//'`
+volume=`pacmd list-sinks|grep "volume: 0:"|cut -c 13-15`
 
 # calculate new volume (+/- 2%)
 volume=$((volume $1 2 ))
+
+if [ $volume -gt 100 ]; then
+    volume=$((100))
+elif [ $volume -lt 0 ]; then
+    volume=$((0))
+fi
 
 # change volume
 pactl set-sink-volume $sink "$volume%"
