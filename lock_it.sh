ae208d219328b1a85a36367875d113ce3192e46f
Andreas Geisenhainer <psycorama@datenhalde.de>
2022-03-10 10:14:15 +0100

    feat(lock screen): update to use `i3lock` and comment out layout change
     * I no longer need to switch layouts to unlock my screen, but keep
       commands

--- lock_it.sh
+++ lock_it.sh
@@ -3,6 +3,6 @@
 # switches keyboard layout before activating screensaver
 # and switches back after succesfull authentification
 #
-$HOME/scripte/keyboard_settings.sh us
-xlock -mode matrix
-$HOME/scripte/keyboard_settings.sh bone
+#$HOME/scripte/keyboard_settings.sh us
+i3lock -c 226622
+#$HOME/scripte/keyboard_settings.sh bone
