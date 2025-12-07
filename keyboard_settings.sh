6f6624d89265b094fcf0dbb7ec8ae3f53df19359
Andreas Geisenhainer <psycorama@datenhalde.de>
2025-12-07 19:42:04 +0100

    feat: update settings
     * since this is only working for xserver and I'm transitioning to
       Wayland, this will be kinda the last update for it

--- keyboard_settings.sh
+++ keyboard_settings.sh
@@ -5,29 +5,27 @@
 # or to call whenever your system has some hickups
 # and resets your usb devices ...
 #
-# Copyright 2012+ BSD-2-Clause psycorama@datenhalde.de
+# 2012+ BSD-2-Clause psycorama@datenhalde.de
 #
-#
-MODE=${1}
-SERVER=$(echo ${DISPLAY}|grep -c 2)
+
+# TODO: finish documentation, depends, variables
+MODE=$1
 
 if [ -z "${MODE}" ]; then
     if [ -e "/tmp/bone_${SERVER}" ]; then
-        MODE="us"
+        MODE="de"
     else
-        MODE="bone"
-    fi;
-fi;
+       MODE="bone"
+    fi
+fi
 
 #disables bell
 xset -b
-#sets keyboard timout(ms) and repeat-rate(/s)
-xset r rate 200 35
-# set the mouse-options (speed, button remap)
-"${HOME}/scripte/elecom_remap.sh"
+xset r rate 250 50
 
-NEO_PATH="${HOME}/scripte/neo"
+~/scripte/elecom_remap.sh
 
+NEO_PATH="/home/andy/scripte/neo"
 if xinput --list | grep -q -e "imp tech tasta" -e "psycorama Alya"; then
     if [ -e "/tmp/bone_${SERVER}" ]; then
         rm "/tmp/bone_${SERVER}"
@@ -43,14 +41,15 @@ elif [ "${MODE}" == "bone" ]; then
     xmodmap "${NEO_PATH}/bone.xmodmap"
     # disable key auto-repeat, when using Right-Alt as compose
     # xset -r 48
-elif [ "${MODE}" == "us" ]; then
+
+elif [ "${MODE}" == "us" -o "${MODE}" == "de" ]; then
     if [ -e "/tmp/bone_${SERVER}" ]; then
         rm "/tmp/bone_${SERVER}"
     fi
 
-    setxkbmap us
+    setxkbmap "${MODE}"
     # put Compose-Key on Right-Alt
     # xmodmap -e 'keycode 108 = Multi_key'
-    # enable key auto-repeat
     # xset r 48
+
 fi
